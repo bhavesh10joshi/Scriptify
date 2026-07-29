128a1282af59e8cf7b77c9ea2b94d58d68eb88ce
bhavesh joshi <joshbhavesh10@gmail.com>
2026-07-29 12:21:48 +0530

    fix : made changes in cors

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -12,10 +12,7 @@ dotenv.config({ path: envPath });
 
 const App = express();
 App.use(express.json());
-App.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true
-}));
+App.use(cors());
 
 // Connecting various express api routers to one single express server
 App.use("/Scriptify/Api/User" , UserRouter);
